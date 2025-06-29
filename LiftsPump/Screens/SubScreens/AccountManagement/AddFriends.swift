26b3525827471d447d3099711f91b9c12c7492c6
Ahmed Abushagur <ahmed@abushagur.com>
2025-06-29 02:05:57 -0700

    added friends and etc

---
+++ LiftsPump/Screens/SubScreens/AccountManagement/AddFriends.swift
@@ -0,0 +1,18 @@
+//
+//  AddFriends.swift
+//  LiftsPump
+//
+//  Created by Ahmed Abushagur on 6/29/25.
+//
+
+import SwiftUI
+
+struct AddFriends: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFriends()
+}
